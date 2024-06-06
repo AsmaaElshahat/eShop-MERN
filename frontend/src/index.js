86f1b7604cafc425b361108e3aa1d7a60df7d034
AsmaaElshahat <39085840+AsmaaElshahat@users.noreply.github.com>
2024-06-06 12:49:47 +0100

    adding shopping cart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import store from './store';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+import PrivateRoute from './components/PrivateRoute';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
@@ -13,6 +14,8 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,9 +25,19 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      <Route path='/shipping' element={<ShippingScreen />} />
+
+      {/* Registered users */}
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        {/* <Route path='/order/:id' element={<OrderScreen />} /> */}
+        {/* <Route path='/profile' element={<ProfileScreen />} /> */}
+      </Route>
     </Route>
   )
-)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { updateCart } from '../utils/cartUtils';
 
-const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems: []};
+const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems: [], shippingAddress: {}, paymentMethod: 'payPal'};
 
 const cartSlice = createSlice({
     name: "cart",
@@ -20,14 +20,32 @@ const cartSlice = createSlice({
         removeFromCart: (state, action) => {
             state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
             return updateCart(state);
-        }
+        },
+        saveShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload;
+            return updateCart(state);
+        },
+        savePaymentMethod: (state, action) => {
+            state.paymentMethod = action.payload;
+            return updateCart(state);
+        },
+        clearCartItems: (state, action) => {
+            state.cartItems = [];
+            return updateCart(state);
+        },
+
+        resetCart: (state) => (state = initialState),
+        },
         
-    }
 });
 
 export const {
     addToCart,
     removeFromCart,
+    saveShippingAddress,
+    savePaymentMethod,
+    clearCartItems,
+    resetCart,
   } = cartSlice.actions;
 
 export default cartSlice.reducer;
